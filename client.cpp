2e273460ebf62647b38ad970eab653726ffebfe4
Kylemarchenaguido-star <kylemarchenaguido@gmail.com>
2026-05-03 03:13:50 -0600

    update files again final version 09

--- client.cpp
+++ client.cpp
@@ -59,7 +59,7 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd){
   //how many strings are after the len 
   memcpy(&wbuf[4], &n, 4);
 
-  size_t cur = 8;// skips thw 8 bytes header from before 
+  size_t cur = 8;// skips the 8 bytes header from before 
 
   for (const std::string &s : cmd){
 	uint32_t p = (uint32_t)s.size();
@@ -89,6 +89,7 @@ static int32_t print_response(const uint8_t *data, size_t size){
 	case TAG_NIL:
 		printf("(nil)\n");
 		return 1;
+
 	case TAG_ERR:
 		if (size < 1 + 8){
 			msg("bad response");
@@ -104,26 +105,97 @@ static int32_t print_response(const uint8_t *data, size_t size){
 		}
 		printf("(err) %d %.*s\n", code, len, &data[1 + 8]);
 		return 1 + 8 + len;
+
 	case TAG_STR:
-		/* code */
-		break;
+		if (size < 1 + 4){
+			msg("bad response");
+			return -1;
+		}
+		uint32_t len = 0;
+		memcpy(&len, &data[1], 4);
+		if (size < 1 + 4 + len){
+			msg("bad response");
+			return -1;
+		}
+		printf("(str) %.*s\n", len, &data[1 + 4]);
+		return 1 + 4 + len;
+
 	case TAG_INT:
-		/* code */
-		break;
+			if (size < 1 + 8){
+				msg("bad response");
+				return -1;
+			}
+			int64_t val = 0;
+			memcpy(&val, &data[1], 8);
+			printf("(int) %ld\n", val);
+			return 1 + 8;
+
 	case TAG_DBL:
-		/* code */
-		break;
+			if (size < 1 + 8){
+				msg("bad response");
+				return -1;
+			}
+			double val = 0;
+			memcpy(&val, &data[1], 8);
+			printf("(double) %ld\n", val);
+			return 1 + 8;
+
 	case TAG_ARR:
-		/* code */
-		break;
+		if(size < 1 + 4){
+			msg("bad response");
+			return -1; 
+		}
+		uint32_t len = 0;
+		memcpy(&len, &data[1], 4);
+		printf("(array) len=%u\n", len);
+		size_t arr_bytes = 1 + 4;
+		for (uint32_t i = 0; i < len; i++){
+			int32_t rv = print_response(&data[arr_bytes], size - arr_bytes);
+			if (rv < 0){ return rv;}
+			arr_bytes += (size_t)rv;
+		}
+		printf("(arr) end\n");
+		return (int32_t)arr_bytes;
+
 	default:
-		break;
+		msg("bad response");
+		return -1;
 	}
+}
+
+static int32_t read_res(int fd){
+	char rbuf[4 + k_max_msg];
+	errno = 0;
+	int32_t err = read_full(fd, (uint8_t *)rbuf, 4);
+	if (err){
+		if (errno == 0){msg("EOF");} else {msg("read() error");}
+		return err;
+	}
+	uint32_t len = 0;
+	memcpy(&len, rbuf, 4);
+	if (len > k_max_msg){
+		msg("too long");
+		return -1;
+	}
+
+	err = read_full(fd, (uint8_t *)rbuf[4], len);
+	if (err){
+		msg("read() error");
+		return err;
+	}
+
+	//print the result
+	int32_t rv = print_response((uint8_t *)&rbuf[4], len);
+	if ( rv > 0 && (uint32_t)rv != len){
+		msg("bad response");
+		rv = 1;
+	}
+	return rv;
 }
 
 // This the client side of the server
 
-int main(){
+int main(int argc, char **argv){
 	int fd = socket(AF_INET, SOCK_STREAM,0);
 	if (fd < 0){die("socket()");}
 
@@ -135,22 +207,20 @@ int main(){
 	int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
 	if (rv){die("connect");}
 
-  std::vector<std::string> query_list = {
-    "hola a todos kekers",
-  };
+	std::vector<std::string> cmd;
+	for (int i = 1; i < argc; ++i){
+		cmd.push_back(argv[i]);
+	}
+	int32_t err = send_req(fd, cmd);
+	if (err){
+		goto L_DONE;
+	}
+	err = read_res(fd);
+	if (err){
+		goto L_DONE;
+	}
 
-  for(const std::string &s : query_list){
-    int32_t err = send_req(fd,(uint8_t *)s.data(),s.size());
-    if (err) {goto L_DONE;}
-  }
-  for (size_t i = 0; i < query_list.size(); ++i){
-    int32_t err = read_res(fd);
-    if (err) {goto L_DONE;}
-
-  }
-
-  L_DONE:
-
-  close(fd);
+L_DONE:
+	close(fd);
 	return 0;
 }
